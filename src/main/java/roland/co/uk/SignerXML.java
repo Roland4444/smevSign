2345d27feb0bd08554d11fa1582b544e05ff56fc
Roland4444 <turbopollux@rambler.ru>
2018-06-28 11:54:33 +0000

    reduce code and comments

--- src/main/java/roland/co/uk/SignerXML.java
+++ src/main/java/roland/co/uk/SignerXML.java
@@ -27,7 +27,6 @@ import java.lang.reflect.Field;
 import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-
 import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 public class SignerXML {
@@ -44,7 +43,6 @@ public class SignerXML {
         Transform.register(SmevTransformSpi.ALGORITHM_URN, SmevTransformSpi.class.getName());
         santuarioIgnoreLineBreaks(true);
     }
-
     public static void main(String[] args){
         System.out.print("init JCP");
     }
@@ -74,86 +72,35 @@ public class SignerXML {
         X509Certificate certificate=(X509Certificate)x.getCert();
         PrivateKey privateKey=x.getPrivate();
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        // установка флага, определяющего игнорирование пробелов в
-        // содержимом элементов при обработке XML-документа
         dbf.setIgnoringElementContentWhitespace(true);
-        // установка флага, определяющего преобразование узлов CDATA в
-        // текстовые узлы при обработке XML-документа
         dbf.setCoalescing(true);
-        // установка флага, определяющего поддержку пространств имен при
-        // обработке XML-документа
         dbf.setNamespaceAware(true);
-// загрузка содержимого подписываемого документа на основе
-        // установленных флагами правил из массива байтов data            DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new ByteArrayInputStream(data));
-        /*
-         * Добавление узла подписи <ds:Signature> в загруженный XML-документ
-         */
-        // алгоритм подписи (ГОСТ Р 34.10-2001)
         final String signMethod = XMLDSIG_MORE_GOSTR34102001_GOSTR3411;
-        // алгоритм хеширования, используемый при подписи (ГОСТ Р 34.11-94)
         final String digestMethod = XMLDSIG_MORE_GOSTR3411;
         final String canonicalizationMethod = CANONICALIZATION_METHOD;
-        String[][] filters = {{XPath2FilterContainer.SUBTRACT, DS_SIGNATURE}};
         String sigId = SIG_ID;
-        // инициализация объекта формирования ЭЦП в соответствии с
-        // алгоритмом ГОСТ Р 34.10-2001
         XMLSignature sig = new XMLSignature(doc, "", signMethod, canonicalizationMethod);
-        // определение идентификатора первого узла подписи
         sig.setId(sigId);
-        // получение корневого узла XML-документа
         Element anElement = null;
-        if (xmlElementName == null) {
+        if (xmlElementName == null)
             anElement = doc.getDocumentElement();
-        } else {
+        else {
             NodeList nodeList = doc.getElementsByTagName(xmlElementName);
             anElement = (Element) nodeList.item(0);
         }
-        // = doc.getElementById("#AppData");
-        // добавление в корневой узел XML-документа узла подписи
-        if (anElement != null) {
+        if (anElement != null)
             anElement.appendChild(sig.getElement());
-        } else {
-           // throw new SignatureProcessorException(COULD_NOT_FIND_XML_ELEMENT_NAME + xmlElementName);
-        }
-        /*
-         * Определение правил работы с XML-документом и добавление в узел подписи этих
-         * правил
-         */
-        // создание узла преобразований <ds:Transforms> обрабатываемого
-        // XML-документа
         Transforms transforms = new Transforms(doc);
-        // добавление в узел преобразований правил работы с документом
-        // transforms.addTransform(Transforms.TRANSFORM_ENVELOPED_SIGNATURE);
         transforms.addTransform(Transforms.TRANSFORM_C14N_EXCL_OMIT_COMMENTS);
         transforms.addTransform(SmevTransformSpi.ALGORITHM_URN);
-        // добавление в узел подписи ссылок (узла <ds:Reference>),
-        // определяющих правила работы с
-        // XML-документом (обрабатывается текущий документ с заданными в
-        // узле <ds:Transforms> правилами
-        // и заданным алгоритмом хеширования)
         sig.addDocument(xmlElementID == null ? "" : GRID + xmlElementID, transforms, digestMethod);
-        /*
-         * Создание подписи всего содержимого XML-документа на основе закрытого ключа,
-         * заданных правил и алгоритмов
-         */
-
-        // создание внутри узла подписи узла <ds:KeyInfo> информации об
-        // открытом ключе на основе
-        // сертификата
         sig.addKeyInfo(certificate);
-        // создание подписи XML-документа
         sig.sign(privateKey);
-        // определение потока, в который осуществляется запись подписанного
-        // XML-документа
         ByteArrayOutputStream bais = new ByteArrayOutputStream();
-        // инициализация объекта копирования содержимого XML-документа в
-        // поток
         TransformerFactory tf = TransformerFactory.newInstance();
-        // создание объекта копирования содержимого XML-документа в поток
         Transformer trans = tf.newTransformer();
-        // копирование содержимого XML-документа в поток
         trans.transform(new DOMSource(doc), new StreamResult(bais));
         bais.close();
         return bais.toByteArray();
